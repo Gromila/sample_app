93660a1a2e7ef01478590f1cb370ddef98e8da88
Gromila <gromilich@gmail.com>
2014-01-11 03:28:47 -0800

    Edited root route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ SampleApp::Application.routes.draw do
   
   resources :users
   
-  root to: "static_pages#home"
+  root "static_pages#home"
   
   get '/signup',    to: "users#new"
   get '/help',      to: "static_pages#help"
